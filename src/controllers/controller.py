b4444317c13095b34ae9f93094c23bd0cb02722c
Akash Verma <aakashverma7184@gmail.com>
2023-09-11 01:05:56 +0530

    Implement validate guess API
    - API takes two query parameter
    - Change the ODM to mongoengine
    - Add ODM for video model

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -1,6 +1,8 @@
-from flask import Response
+from flask import Response, request
 from flask_restful import Resource
+import json
 from ..services.s3_service import S3Service
+from ..services.video_service import VideoService
 
 
 class FetchClip(Resource):
@@ -13,3 +15,30 @@ class FetchClip(Resource):
             )
         except Exception as e:
             return str(e), 404
+
+
+class ValidateGuess(Resource):
+    def get(self):
+        guess = request.args.get('guess')
+        video_key = request.args.get('key')
+
+        print(guess)
+        print(video_key)
+
+        video_service = VideoService()
+        video = video_service.get_video_by_key(video_key).to_json()
+        data = json.loads(video)
+        video_name = data["name"]
+
+        if guess == video_name:
+            print("Is correct cnd ran")
+            print(True)
+            response = {"valid": True}
+            valid_json = json.dumps(response)
+            return valid_json, 200
+        else:
+            print("Isnt correct cnd ran")
+            print(False)
+            response = {"valid": False}
+            invalid_json = json.dumps(response)
+            return invalid_json, 200
--- src/server.py
+++ src/server.py
@@ -1,18 +1,23 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
-from flask_pymongo import PyMongo
-import os
-from .controllers.controller import FetchClip
+from flask_mongoengine import MongoEngine
+from .controllers.controller import FetchClip, ValidateGuess
 from .config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 api = Api(app)
 
-mongo = PyMongo(app)
-db = mongo.db
+db = MongoEngine(app)
+
+cors = CORS(app, resources={
+    r"/video/*": {"origins": "http://localhost:5173"},
+    r"/validate-guess*": {"origins": "http://localhost:5173"}
+})
 
 api.add_resource(FetchClip, "/video/<string:video_key>")
+api.add_resource(ValidateGuess, "/validate-guess")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
